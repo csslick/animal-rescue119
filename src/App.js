eb3b5bc9582fa54e4188889de65d330376bc97d9
JuHwan <juhwan203@kakao.com>
2023-02-01 10:51:01 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react'
 
 function App() {
+  const API_KEY = `sXxYjidiN3t6GurP%2FlL532W8cmmt4qCl%2F%2BFF72uNIWACqGGmumk6enycmK39NmiGxpmGhhxqFXvWYu4zH8f3zg%3D%3D`;
+  const API_URL = `http://apis.data.go.kr/6260000/BusanPetAnimalInfoService/getPetAnimalInfo?serviceKey=${API_KEY}&numOfRows=10&pageNo=1&resultType=json`;
+
+  const getAnimalData = () => {
+    fetch(API_URL)
+      .then(res => res.json())
+      .then(data => {
+        console.log(data.getPetAnimalInfo.body.items)
+        const items = data.getPetAnimalInfo.body.items;
+      })
+  }
+
+  useEffect(() => {
+    getAnimalData();
+  }, [])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <h1>부산시 동물구조 현황</h1>
   );
 }
 
