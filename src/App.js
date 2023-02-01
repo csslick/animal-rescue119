8386691bdba9b736d159dde32103f138776d5b2c
JuHwan <juhwan203@kakao.com>
2023-02-01 15:38:29 +0900

    https경로 수정-보안 이슈

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Items from './components/Items'
 
 function App() {
   const API_KEY = `sXxYjidiN3t6GurP%2FlL532W8cmmt4qCl%2F%2BFF72uNIWACqGGmumk6enycmK39NmiGxpmGhhxqFXvWYu4zH8f3zg%3D%3D`;
-  const API_URL = `http://apis.data.go.kr/6260000/BusanPetAnimalInfoService/getPetAnimalInfo?serviceKey=${API_KEY}&numOfRows=10&pageNo=1&resultType=json`;
+  const API_URL = `https://apis.data.go.kr/6260000/BusanPetAnimalInfoService/getPetAnimalInfo?serviceKey=${API_KEY}&numOfRows=10&pageNo=1&resultType=json`;
   const [animals, setAnimals] = useState([])
 
   const getAnimalData = () => {
