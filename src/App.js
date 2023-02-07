3d33924928c8ec3adb7d1b10dc056cb4ddb30350
JuHwan <juhwan203@kakao.com>
2023-02-07 14:59:54 +0900

    페이징 기능 추가

--- src/App.js
+++ src/App.js
@@ -3,14 +3,15 @@ import { useState, useEffect } from 'react'
 import Items from './components/Items'
 
 function App() {
-  const [animals, setAnimals] = useState([])
+  const [animals, setAnimals] = useState([]) // 동물 데이터
   const [totalCount, setTotalCount] = useState(0); // 총 글수
   const [pageNo, setPageNo] = useState(1);  // 페이지번호
-  const [maxPages, setMaxPages] = useState(0); //
+  const [maxPages, setMaxPages] = useState(0); // 마지막 페이지
+  const [rows, setRows] = useState(10); // 화면당 글수
 
   const getAnimalData = () => {
     const API_KEY = `sXxYjidiN3t6GurP%2FlL532W8cmmt4qCl%2F%2BFF72uNIWACqGGmumk6enycmK39NmiGxpmGhhxqFXvWYu4zH8f3zg%3D%3D`;
-    const API_URL = `https://apis.data.go.kr/6260000/BusanPetAnimalInfoService/getPetAnimalInfo?serviceKey=${API_KEY}&numOfRows=10&pageNo=${pageNo}&resultType=json`;    
+    const API_URL = `https://apis.data.go.kr/6260000/BusanPetAnimalInfoService/getPetAnimalInfo?serviceKey=${API_KEY}&numOfRows=${rows}&pageNo=${pageNo}&resultType=json`;    
     fetch(API_URL)
       .then(res => res.json())
       .then(data => {
@@ -20,25 +21,27 @@ function App() {
         setAnimals(items);
         // 전체글
         setTotalCount(data.getPetAnimalInfo.body.totalCount);
-        // 마지막 페이지
-        setMaxPages(Math.ceil(totalCount / 10))
+        // 마지막 페이지(총게시물 / 행수)
+        setMaxPages(Math.ceil(totalCount / rows))
       })
       .catch(() => {
         console.log('데이터 요청 에러')
       })
   }
 
+  // 이전 페이지
   const nextPage = () => {
     if(pageNo < maxPages) setPageNo(pageNo + 1)
   }
 
+  // 다음 페이지
   const prevPage = () => {
-    if(pageNo > 0) setPageNo(pageNo - 1);
+    if(pageNo > 1) setPageNo(pageNo - 1);
   }
 
   useEffect(() => {
     getAnimalData();
-  }, [pageNo])
+  }, [pageNo, totalCount])
 
   console.log('animals = ', animals)
   console.log('totalCount = ', totalCount)
@@ -47,6 +50,7 @@ function App() {
   return (
     <div className='App'>
       <h1>동물구조 현황(부산)</h1>
+      {/* 페이징 */}
       <p>Page: {pageNo} / {maxPages}</p>
       <p>{totalCount}건 등록</p>
       <button onClick={prevPage}>이전 페이지</button>
@@ -54,7 +58,11 @@ function App() {
       {
         (animals.length > 0) ?  <Items animals={animals} /> : (<p>로딩중...</p>)
       }
-      <button>다음 페이지</button>
+      {/* 페이징 */}
+      <p>Page: {pageNo} / {maxPages}</p>
+      <p>{totalCount}건 등록</p>
+      <button onClick={prevPage}>이전 페이지</button>
+      <button onClick={nextPage}>다음 페이지</button>
     </div>
   );
 }
